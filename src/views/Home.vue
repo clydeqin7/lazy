3f1eec3d564ab91f400759bfd096d7cc099eab2e
clydeqin7 <clydeqin7@foxmail.com>
2020-11-14 19:25:25 +0800

    topnav组件；home页样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <div class="menu">
-           <li>菜单1</li>
-        <li>菜单2</li>
-      </div>
-    </div>
+    <Topnav/>
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -17,21 +11,33 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Topnav from "../components/Topnav.vue"
+export default {
+  components: { Topnav }
+}
+</script>
+
 <style lang="scss" scoped>
-  .topnav {
-  background: pink;
+.banner {
+  padding: 100px 0;
   display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
   }
 }
