6e386b95b218add82155caf227eab8520cf373e9
clydeqin7 <clydeqin7@foxmail.com>
2020-11-14 17:32:28 +0800

    搭建html结构

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <nav>导航栏
-    <router-link to="/">菜单1</router-link>
-    <router-link to="/xxx">菜单2</router-link>
-  </nav>
-  <hr>
   <router-view></router-view>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory, createRouter} from "vue-router"
-import menu1 from './components/menu1.vue'
-import menu2 from './components/menu2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
   history,
   routes: [
-    { path: '/', component: menu1 },
-    { path: '/xxx', component: menu2}
+    { path: '/', component: Home },
+    { path: '/doc', component: Doc }
   ]
 })
 
